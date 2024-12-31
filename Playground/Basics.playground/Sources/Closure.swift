5978f2f7cbb044fe52dfc82f2fc5eced7f2d7b4b
saebyeokjang <dev.saebyeok@gmail.com>
2024-12-31 16:47:10 +0900

    Fix

--- Playground/Basics.playground/Sources/Closure.swift
+++ Playground/Basics.playground/Sources/Closure.swift
@@ -6,25 +6,25 @@
 //
 import Foundation
 
-func performOperation(_ a: Int, _ b: Int, operation: (Int, Int) -> Int {
-    return operation(a, b)
-})
+//func performOperation(_ a: Int, _ b: Int, operation: (Int, Int) -> Int {
+//    return operation(a, b)
+//})
 
-let multiply: (Int, Int) -> Int = { $0 * $1 }
-let addition: (Int, Int) -> Int = { $0 + $1 }
+//let multiply: (Int, Int) -> Int = { $0 * $1 }
+//let addition: (Int, Int) -> Int = { $0 + $1 }
 
-let result = performOperation(5, 6, operation: multiply)
-let result2 = performOperation(5, 6, operation: addition)
+//let result = performOperation(5, 6, operation: multiply)
+//let result2 = performOperation(5, 6, operation: addition)
 
 // 인라인 함수 정의
-let result3 = performOperation(5, 6, operation: { x, y in x - y })
-print(result3)
+//let result3 = performOperation(5, 6, operation: { x, y in x - y })
+//print(result3)
 
 // 후행 클로저
 let numbers: [Int] = [1, 2, 3, 4, 5]
 //let squaredNumbers: numbers.map { element in element * element}
 let squaredNumbers = numbers.map { $0 * $0 }
-print(squaredNumbers)
+//print(squaredNumbers)
 
 func makeIncrementer(incrementAmount: Int) -> () -> Int {
     var total = 0
@@ -35,6 +35,6 @@ func makeIncrementer(incrementAmount: Int) -> () -> Int {
     return incrementer
 }
 
-let incrementer = makeIncrementer(incrementAmount: 5)
-print(incrementer()) //5씩 증가
-print(incrementer()) //5씩 증가
+//let incrementer = makeIncrementer(incrementAmount: 5)
+//print(incrementer()) //5씩 증가
+//print(incrementer()) //5씩 증가
