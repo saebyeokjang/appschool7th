3fcf31c6a29e0b1402d25846c30835f7b49fdf34
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-06 13:11:47 +0900

    URLSession 실습

--- Playground/URLSession.playground/Contents.swift
+++ Playground/URLSession.playground/Contents.swift
@@ -3,5 +3,19 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 import Foundation
+import UIKit
 
 let config = URLSessionConfiguration.default
+let session = URLSession(configuration: config)
+
+let urlString = "https://imgs.xkcd.com/comics/api.png"
+let url = URL(string: urlString)!
+let request = URLRequest(url: url)
+
+let task = session.dataTask(with: request) { data, response, error in
+    // 요청에 대한 처리
+    guard let imageData = data else { return }
+    _ = UIImage(data: imageData)
+}
+
+task.resume()
