69a42644c93cb148d6adc1627f8b267caf35bf95
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-07 11:05:29 +0900

    튜플

--- SwiftUI/StringType.playground/Contents.swift
+++ SwiftUI/StringType.playground/Contents.swift
@@ -47,3 +47,29 @@ print(backSlash)
 let escapedString =
 "줄바꿈: \n, 캐리지 리턴: \r, 탭: \t, 역슬래시: \\, 따옴표: \", 작은따옴표: ', 유니코드: \u{FF}"
 print(escapedString)
+
+let firstIndex = greetings.startIndex
+let lastIndex = greetings.index(before: greetings.endIndex)
+print(greetings[firstIndex])
+print(greetings[lastIndex])
+
+// 문자열 슬라이싱
+// print(greetings[firstIndex+1])
+// 숫자를 더하는 방식으로 사용할 수 없음 String.Index라는 타입임
+let range = greetings.index(firstIndex, offsetBy: 1)..<greetings.index(lastIndex, offsetBy: -1)
+print(greetings[range])
+
+// 변수 var 는 수정 가능
+backSlash = "\\ \(backSlash)"
+print(backSlash)
+
+let maxLength = 10
+
+let myTuple = (432, 3.14, "This is a String")
+//let myString = myTuple.2
+let (_, _, myString) = myTuple
+print(myString)
+
+let myTuple2 = (count: 10, length: 432.433, message: "This is a String")
+
+print(myTuple2.message)
