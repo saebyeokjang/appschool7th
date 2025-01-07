ed4e495842ee8a661f7bcc2c98db3b71624627e9
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-07 10:08:42 +0900

    String 1

---
+++ SwiftUI/StringType.playground/Contents.swift
@@ -0,0 +1,39 @@
+
+let greetings = "Hello, World!"
+print(greetings)
+
+// 여러줄의 문자열, 들여쓰기 가능
+let multipleLines = """
+동해물과 백두산이 마르고 닳도록
+    하느님이 보우하사 우리나라
+        만세
+"""
+print(multipleLines)
+
+// 문자열 보간법
+let name = "Swift"
+let welcomeMessage = "Hello, \(name)!"
+print(welcomeMessage)
+
+let romeoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
+
+var act1SceneCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasPrefix("Act 1") {
+        act1SceneCount += 1
+    }
+}
+
+print("There are \(act1SceneCount) scenes in Act 1.")
