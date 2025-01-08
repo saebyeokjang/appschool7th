a7399db1df2b351464e9f112928df8b984e52137
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-08 16:04:32 +0900

    HelloWorld app project

---
+++ SwiftUI/HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 장새벽 on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
