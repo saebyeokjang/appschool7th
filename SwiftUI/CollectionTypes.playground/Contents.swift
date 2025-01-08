af01af7fee4724332e60ef83279291b1aa5067de
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-08 14:22:13 +0900

    Dictionary

--- SwiftUI/CollectionTypes.playground/Contents.swift
+++ SwiftUI/CollectionTypes.playground/Contents.swift
@@ -60,3 +60,31 @@ for object in mixedArray {
         print("stringValue: \(stringValue)")
     }
 }
+
+// Dictionary
+var bookDict1 = [
+    "100-432112": "Wind in the Willows",
+    "200-532874": "Tale of Two Cities",
+    "202-546549": "Sense and Sensibility",
+    "104-109834": "Shutter Island"
+]
+
+var myDictionary = [Int: String]()
+
+let keys = ["100-432112", "200-532874", "202-546549", "104-109834"]
+let values = ["Wind in the Willows", "Tale of Two Cities", "Sense and Sensibility", "Shutter Island"]
+
+var bookDict = Dictionary(uniqueKeysWithValues: zip(keys, values))
+print(bookDict.count)
+print(bookDict["200-532874"] ?? "Book not found")
+
+bookDict.updateValue("The Ruins", forKey: "200-532874")
+
+bookDict["300-898871"] = "The Overlook"
+
+bookDict["300-898871"] = nil // 값 제거
+bookDict.removeValue(forKey: "300-898871")
+
+for (key, value) in bookDict {
+    print("key: \(key), value: \(value)")
+}
