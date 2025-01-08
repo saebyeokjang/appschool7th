1c9bdf8ec696ca469b57b0a47d5e19214f2b45d9
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-08 10:34:06 +0900

    값타입과 참조타입

---
+++ SwiftUI/StructEnum.playground/Contents.swift
@@ -0,0 +1,41 @@
+
+struct SmapleStruct {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func buildHelloMsg() -> String {
+        /* return */"Hello \(name)"
+        // 함수의 한줄 실행구문의 결과가 함수의 반환타입과 일치하는 경우
+        // return 키워드 생략 가능
+    }
+}
+
+class SampleClass {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func buildHelloMsg() -> String {
+        "Hello \(name)"
+        
+    }
+}
+
+// 값 타입
+let myStruct1 = SmapleStruct(name: "구조체 초기 이름")
+var myStruct2 = myStruct1
+myStruct2.name = "구조체 이름 바꿈"
+print(myStruct1.name)
+print(myStruct2.name)
+
+// 참조 타입
+let myClass1 = SampleClass(name: "클래스 초기 이름")
+var myClass2 = myClass1
+myClass2.name = "클래스 이름 바꿈"
+print(myClass1.name)
+print(myClass2.name)
