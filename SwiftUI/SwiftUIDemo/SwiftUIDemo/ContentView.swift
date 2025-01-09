6faa757645cdaabe14b59c53c05bf41d877b7ec1
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-09 21:35:11 +0900

    Lab8 과제

--- SwiftUI/SwiftUIDemo/SwiftUIDemo/ContentView.swift
+++ SwiftUI/SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -8,44 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
-    var colors: [Color] = [.black, .red, .blue, .green]
-    var colornames = ["Black", "Red", "Blue", "Green"]
+    @State private var selectedNumberIndex = 0
+    @State private var count = 0
+    let numbers = ["1", "5", "10"]
     
-    @State private var colorIndex: Int = 0
-    @State private var rotation: Double = 0
-    @State private var text: String = "Welcome to SwiftUI"
+    var selectedNumber: Int {
+        return Int(numbers[selectedNumberIndex]) ?? 0
+    }
     
     var body: some View {
         VStack {
-            Spacer()
-            Text(text)
+            Text("단위를 선택하세요")
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .rotationEffect(.degrees(rotation))
-                .animation(.easeInOut(duration: 3), value: rotation)
-                .foregroundColor(colors[colorIndex])
-            Spacer()
-            Divider()
             
-            Slider(value: $rotation, in: 0...360, step: 0.1)
-                .padding()
-            
-            TextField("Enter text here", text: $text)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            Picker(selection: $colorIndex, label: Text("Color")) {
-                ForEach (0 ..< colornames.count, id:\.self) {
-                    Text(colornames[$0])
-                        .foregroundStyle(colors[$0])
+            Picker("Number", selection: $selectedNumberIndex) {
+                ForEach(0..<numbers.count, id: \.self) { index in
+                    Text(self.numbers[index])
                 }
             }
             .pickerStyle(.wheel)
-            .padding()
+            
+            Text("Count")
+                .font(.largeTitle)
+            
+            Text("\(count)")
+                .font(.system(size: 100))
+                .fontWeight(.bold)
+            
+            HStack {
+                Button(action: {
+                    self.count -= selectedNumber
+                }, label: {
+                    Text("감소")
+                        .font(.title2)
+                        .padding()
+                        .background(Color.red)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                })
+                
+                Spacer()
+                
+                // "초기화" 버튼
+                Button(action: {
+                    self.count = 0  // 카운트를 0으로 초기화
+                }) {
+                    Text("초기화")
+                        .font(.title3)
+                        .padding()
+                        .background(Color.black)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    self.count += selectedNumber
+                }) {
+                    Text("증가")
+                        .font(.title2)
+                        .padding()
+                        .background(Color.green)
+                        .foregroundColor(.white)
+                        .clipShape(Circle())
+                }
+            }
         }
+        .padding()
     }
 }
-
 #Preview {
     ContentView()
 }
