7aae36af2eda8d01526fd1076a9d80061a6ffaaa
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-09 14:46:57 +0900

    동적 레이아웃, 텍스트 줄바뀜 제한, 우선순위

--- SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -7,80 +7,36 @@
 
 import SwiftUI
 
-struct MyVStack<Content: View>: View {
-    let content: () -> Content
-    
-    init(@ViewBuilder content: @escaping () -> Content) {
-        self.content = content
-    }
+struct ContentView: View {
+    @State var myLayout: AnyLayout = AnyLayout(VStackLayout())
     
     var body: some View {
-        VStack(spacing: 10) {
-            content()
+        myLayout {
+            Text("Sample Text")
+            Text("Sample Text")
+            Text("Sample Text")
+        }
+        HStack {
+            Button(action: {
+                myLayout = AnyLayout(HStackLayout())
+            }) {
+                Text("HStack")
+            }
+            Button(action: {
+                myLayout = AnyLayout(VStackLayout())
+            }) {
+                Text("VStack")
+            }
+        }
+        
+        HStack {
+            Image(systemName: "airplane")
+            Text("Flight times:")
+                .layoutPriority(1) // 우선순위 부여
+            Text("London")
         }
         .font(.largeTitle)
-    }
-}
-
-// 커스텀 레이블 스타일 (레이아웃)
-struct VerticalLabelStyle: LabelStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        VStack(alignment: .center) {
-            configuration.icon
-                .padding(5)
-            configuration.title
-        }
-    }
-}
-
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            HStack {
-                Image(systemName: "goforward.10")
-                Image(systemName: "goforward.15")
-                Image(systemName: "goforward.30")
-            }
-            ZStack {
-                Image(systemName: "goforward.10")
-                Image(systemName: "goforward.15")
-                Image(systemName: "goforward.30")
-            }
-            // 복잡한 중첩 레이아웃 예시
-            Text("Financial Results")
-                .font(.title)
-            HStack {
-                Text("Q1 Sales")
-                    .font(.headline)
-                VStack {
-                    Text("January")
-                    Text("February")
-                    Text("March")
-                }
-                VStack {
-                    Text("$1000")
-                    Text("$200")
-                    Text("$3000")
-                }
-            }
-            // 정렬, 여백
-            HStack(alignment: .top) {
-                Text("Q1 Sales")
-                    .font(.headline)
-                Spacer()
-                VStack(alignment: .center) {
-                    Text("January")
-                    Text("February")
-                    Text("March")
-                }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("$1000")
-                    Text("$200")
-                    Text("$3000")
-                }
-            }
-        }
+        .lineLimit(1) // 글씨 크기로 인해 줄 바뀜을 제한
     }
 }
 
