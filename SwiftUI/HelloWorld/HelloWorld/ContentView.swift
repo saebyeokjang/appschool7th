16bfa7bf79241bb610dbd51fe02beba24982b291
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-09 10:32:42 +0900

    VStack, HStack, Text

--- SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        // 수직 배열 레이아웃
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Text1")
+            Text("Text2")
+            Text("Text3")
+            // 수평 배열 레이아웃
+            HStack {
+                Text("Text4")
+                Text("Text5")
+                // 텍스트 뷰는 + 연산자로 이어붙일 수 있다
+                Text("Hello, ") + Text("World!")
+            }
+            Text("Text6")
+            Text("Hello, ") + Text("World!")
         }
-        .padding()
     }
 }
 
