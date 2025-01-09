ed4f6222eda6cb6f087bb065c8cee85c1603cc16
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-09 15:04:31 +0900

    Clear

--- SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -8,35 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var myLayout: AnyLayout = AnyLayout(VStackLayout())
     
     var body: some View {
-        myLayout {
-            Text("Sample Text")
-            Text("Sample Text")
-            Text("Sample Text")
-        }
-        HStack {
-            Button(action: {
-                myLayout = AnyLayout(HStackLayout())
-            }) {
-                Text("HStack")
-            }
-            Button(action: {
-                myLayout = AnyLayout(VStackLayout())
-            }) {
-                Text("VStack")
-            }
-        }
-        
-        HStack {
-            Image(systemName: "airplane")
-            Text("Flight times:")
-                .layoutPriority(1) // 우선순위 부여
-            Text("London")
-        }
-        .font(.largeTitle)
-        .lineLimit(1) // 글씨 크기로 인해 줄 바뀜을 제한
+        Spacer()
     }
 }
 
