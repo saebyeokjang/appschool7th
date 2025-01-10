e6ea218269d1c6e52a1d376ba37bfd26247e80c6
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-10 11:23:05 +0900

    작업 그룹

--- SwiftUI/HelloWorld/HelloWorld/ContentView.swift
+++ SwiftUI/HelloWorld/HelloWorld/ContentView.swift
@@ -7,13 +7,31 @@
 
 import SwiftUI
 
+enum DurationError: Error {
+    case tooLong
+    case tooShort
+}
+
 struct ContentView: View {
-    
     var body: some View {
-        
         Button(action: {
-            Task {
+            // 작업의 우선순위를 결정할 수 있다.
+            Task(priority: .high) {
+                // 작업의 우선순위를 확인하는 프로퍼티
+                _ = Task.currentPriority
+                // 작업의 취소 여부를 확인하는 프로퍼티
+                _ = Task.isCancelled
+                await Task.yield()
                 await doSomething()
+            }
+            // 분리된 작업
+            let detachedTask = Task.detached {
+                await doSomething()
+            }
+            
+            if(!detachedTask.isCancelled) {
+                // 작업 취소
+                detachedTask.cancel()
             }
         }) {
             Text("Do Something")
@@ -21,20 +39,20 @@ struct ContentView: View {
     }
     
     func doSomething() async {
-        print("Start \(Date())")
-        // 비동기 반환값을 위한 async let
-        async let result = takesTooLong()
-        print("After async-let \(Date())")
-        // async let 상수를 사용하려면, await
-        print("result \(await result)")
-        print("End \(Date())")
+        await withTaskGroup(of: Void.self) { group in
+            for i in 1...5 {
+                group.addTask {
+                    let result = await takesTooLong()
+                    print("Completed Task \(i) = \(result)")
+                }
+            }
+        }
     }
     
     func takesTooLong() async -> Date {
         sleep(5)
         return Date()
     }
-    
 }
 
 #Preview {
