a9de3750fe75e0ce743dcff2f88d36d5ff441595
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-13 10:20:03 +0900

    scenePhase

--- SwiftUI/HelloWorld/HelloWorld/HelloWorldApp.swift
+++ SwiftUI/HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct HelloWorldApp: App {
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase, initial: true) {
+            switch scenePhase {
+            case .active:
+                print("Active")
+            case .inactive:
+                print("Inactive")
+            case .background:
+                print("Background")
+            default:
+                print("Unknown")
+            }
         }
     }
 }
