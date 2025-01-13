2f44a3a6da43b36dd07b1eea7adb43caea4e0fa1
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-13 15:24:34 +0900

    @SceneStorage를 이용한 데이터 유지

---
+++ SwiftUI/StorageDemo/StorageDemo/StorageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  StorageDemoApp.swift
+//  StorageDemo
+//
+//  Created by 장새벽 on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct StorageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
