748088d4b04abd71a139f12cf8abccc76c7bdd06
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-13 14:29:31 +0900

    Fix

--- SwiftUI/ObservableDemo/ObservableDemo/TimerData.swift
+++ SwiftUI/ObservableDemo/ObservableDemo/TimerData.swift
@@ -15,14 +15,15 @@ class TimerData {
     var timer: Timer?
     
     init() {
-        //      기존 셀렉터 호출 방식 : #selector 필요
-        //        timer = Timer.scheduledTimer(timeInterval: 1.0,
-        //                                     target: self,
-        //                                     selector: #selector(timerDidFire),
-        //                                     userInfo: nil,
-        //                                     repeats: true)
-        
-        // [weak self] 는 변수 캡쳐 시 ARC 카운트가 증가하지 않도록 해서, 메모리 누수를 막는다.
+        /*
+        기존 셀렉터 호출 방식 : #selector 필요
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                     target: self,
+                                     selector: #selector(timerDidFire),
+                                     userInfo: nil,
+                                     repeats: true)
+         */
+        // [weak self] 는 변수 캡쳐 시 ARC 카운트가 증가하지 않도록 해서, 메모리 누수를 막는다
         // [unowned self] 는 강제 언래핑된 self 키워드, 조심해서 사용해야함
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [unowned self] _ in
             self.timerDidFire()
