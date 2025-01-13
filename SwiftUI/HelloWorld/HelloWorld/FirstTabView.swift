df651ef1af509877251352020f1ad81b8d997591
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-13 11:04:35 +0900

    task modifier

--- SwiftUI/HelloWorld/HelloWorld/FirstTabView.swift
+++ SwiftUI/HelloWorld/HelloWorld/FirstTabView.swift
@@ -8,8 +8,10 @@
 import SwiftUI
 
 struct FirstTabView: View {
+    @State private var title: String = "View One"
+    
     var body: some View {
-        Text("View One")
+        Text(title)
             // 화면에 이 뷰가 보여줄 때 호출되는 수정자
             .onAppear(perform: {
                 print("onAppear triggered")
@@ -18,7 +20,21 @@ struct FirstTabView: View {
             .onDisappear(perform: {
                 print("onDisappear triggered")
             })
+            .task(priority: .background) {
+                print("Task executed!")
+                title = await changeTitle()
+            }
     }
+    
+    func changeTitle() async -> String {
+        do {
+            try await Task.sleep(nanoseconds: 5_000_000_000)
+        } catch {
+            print("Error: \(error)")
+        }
+        return "Async task completed"
+    }
+    
 }
 
 #Preview {
