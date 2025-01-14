fd21637a0382c9d95dc5cd655366aa2a19cb9ccd
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-14 09:42:33 +0900

    스택과 정렬

--- SwiftUI/StackDemo/StackDemo/ContentView.swift
+++ SwiftUI/StackDemo/StackDemo/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        // ailgnment 파라미터로 정렬
+        VStack(alignment: .leading, spacing: 10) {
+            Text("This is some text")
+            Text("This is some longer text")
+            Text("This is short")
         }
-        .padding()
+        HStack(alignment: .lastTextBaseline, spacing: 20) {
+            Text("This is some text")
+                .font(.largeTitle)
+            Text("This is some longer text")
+                .font(.body)
+            Text("This is short")
+                .font(.headline)
+        }
+        VStack(alignment: .leading) {
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: 120, height: 50)
+            Rectangle()
+                .fill(Color.red)
+                .alignmentGuide(.leading) { d in d.width / 3 }
+                .frame(width: 200, height: 50)
+            Rectangle()
+                .fill(Color.blue)
+                .alignmentGuide(.leading) { d in d[.trailing] + 20 }
+                .frame(width: 180, height: 50)
+        }
     }
 }
 
