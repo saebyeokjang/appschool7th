5d8eb44e2694c5a7b31ab0729917006c2645ef6f
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-15 13:28:32 +0900

    Context menu

---
+++ SwiftUI/ContextMenuDemo/ContextMenuDemo/ContextMenuDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuDemoApp.swift
+//  ContextMenuDemo
+//
+//  Created by 장새벽 on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
