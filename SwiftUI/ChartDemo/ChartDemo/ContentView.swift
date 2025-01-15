ae4cb0d8ef2ffd4f6216404b0a2314672af500b8
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-15 15:48:53 +0900

    차트 배경, 보간법 변경

--- SwiftUI/ChartDemo/ChartDemo/ContentView.swift
+++ SwiftUI/ChartDemo/ChartDemo/ContentView.swift
@@ -12,22 +12,41 @@ struct MonthlyTemp: Identifiable {
     var id = UUID()
     var month: String
     var degrees: Int
+    var year: String
 }
 
 let tempData: [MonthlyTemp] = [
-    MonthlyTemp(month: "Jan", degrees: 50),
-    MonthlyTemp(month: "Feb", degrees: 43),
-    MonthlyTemp(month: "Mar", degrees: 61)
+    MonthlyTemp(month: "Jan", degrees: 50, year: "2021"),
+    MonthlyTemp(month: "Feb", degrees: 43, year: "2021"),
+    MonthlyTemp(month: "Mar", degrees: 61, year: "2021"),
+    MonthlyTemp(month: "Jan", degrees: 30, year: "2022"),
+    MonthlyTemp(month: "Feb", degrees: 38, year: "2022"),
+    MonthlyTemp(month: "Mar", degrees: 29, year: "2022")
 ]
 
 struct ContentView: View {
     var body: some View {
         Chart(tempData) { data in
-            AreaMark(
+//            AreaMark(
+//                x: .value("Month", data.month),
+//                y: .value("Temp", data.degrees)
+//            )
+//            RectangleMark(
+//                x: .value("Month", data.month),
+//                y: .value("Temp", data.degrees)
+//            )
+            LineMark(
                 x: .value("Month", data.month),
                 y: .value("Temp", data.degrees)
             )
+            .interpolationMethod(.cardinal)
+            .foregroundStyle(by: .value("Year", data.year))
+            .symbol(by: .value("Year", data.year))
         }
+        .chartPlotStyle { plotArea in
+            plotArea.background(.gray.opacity(0.3))
+        }
+        .padding()
     }
 }
 
