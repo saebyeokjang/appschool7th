e35a09985fa830d772928d52aa69c437d581162b
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-15 14:22:03 +0900

    Gesture

---
+++ SwiftUI/GestureDemo/GestureDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GestureDemo
+//
+//  Created by 장새벽 on 1/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
