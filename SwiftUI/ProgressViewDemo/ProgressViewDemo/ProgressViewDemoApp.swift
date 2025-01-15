1f617c6bddfe1e4ee35c016f91be817b6183eca4
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-15 15:01:39 +0900

    ProgressView

---
+++ SwiftUI/ProgressViewDemo/ProgressViewDemo/ProgressViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressViewDemoApp.swift
+//  ProgressViewDemo
+//
+//  Created by 장새벽 on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ProgressViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
