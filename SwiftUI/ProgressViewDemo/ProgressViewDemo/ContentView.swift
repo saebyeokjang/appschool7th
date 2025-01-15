690a2e0d223e87b6d7faa9e096d06b084a1dfeec
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-15 15:11:04 +0900

    기본 ProgressView 스타일

--- SwiftUI/ProgressViewDemo/ProgressViewDemo/ContentView.swift
+++ SwiftUI/ProgressViewDemo/ProgressViewDemo/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var progress: Double = 0.1
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ProgressView("Task Progress", value: progress, total: 100)
+                .progressViewStyle(LinearProgressViewStyle())
+            // macOS 에서만...
+            ProgressView("Task Progress", value: progress, total: 100)
+                .progressViewStyle(CircularProgressViewStyle())
+            ProgressView("Working...")
+            Slider(value: $progress, in: 0...100, step: 0.1)
         }
         .padding()
     }
