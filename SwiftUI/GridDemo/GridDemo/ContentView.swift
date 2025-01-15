2bdbbe0f29c3bf7a19fc42401aeac64840b4cbdd
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-15 11:45:59 +0900

    Grid

--- SwiftUI/GridDemo/GridDemo/ContentView.swift
+++ SwiftUI/GridDemo/GridDemo/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    private var colors: [Color] = [.blue, .yellow, .green]
-    private var gridItem = [GridItem(.adaptive(minimum: 50))]
-    
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: gridItem, spacing: 5) {
-                ForEach(0...99, id: \.self) { index in
-                    CellContent(index: index, color: colors[index % colors.count])
+        Grid(alignment: .leading, verticalSpacing: 15) {
+            GridRow {
+                ForEach(1...5, id: \.self) { index in
+                    if (index % 2 == 1) {
+                        CellContent(index: index, color: .red)
+                    } else {
+                        Color.clear.gridCellUnsizedAxes([.horizontal, .vertical])
+                    }
                 }
             }
+            GridRow {
+                ForEach(6...10, id: \.self) { index in
+                    CellContent(index: index, color: .blue)
+                }
+            }
+            GridRow {
+                ForEach(11...15, id: \.self) { index in
+                    CellContent(index: index, color: .green)
+                }
+            }
+            // Grid 뷰를 추가할 수 있다.
+            CellContent(index: 16, color: .blue)
+
+            // 셀 병합도 가능하다.
+            GridRow {
+                CellContent(index: 17, color: .orange)
+                    .gridCellColumns(2)
+                CellContent(index: 18, color: .indigo)
+                    .gridCellColumns(3)
+            }
         }
+        .padding()
     }
 }
 
