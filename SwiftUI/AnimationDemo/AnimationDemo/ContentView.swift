eacf0b92eb87801646bd9cbe9e6fd8787dc412e1
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-15 14:12:55 +0900

    각종 트랜지션 실습

--- SwiftUI/AnimationDemo/AnimationDemo/ContentView.swift
+++ SwiftUI/AnimationDemo/AnimationDemo/ContentView.swift
@@ -8,27 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var offset: CGSize = .zero
+    @State private var isTextVisible: Bool = false
     
     var body: some View {
-        Circle()
-            .fill(Color.blue)
-            .frame(width: 100, height: 100)
-            .offset(offset)
-            .gesture(
-                DragGesture()
-                    .onChanged {gesture in
-                        withAnimation {
-                            self.offset =
-                            gesture.translation
-                        }
-                    }
-                    .onEnded { _ in
-                        withAnimation(.spring()) {
-                            self.offset = .zero
-                        }
-                    }
-            )
+        VStack(spacing: 20) {
+            Button(action: {
+                withAnimation(.easeInOut(duration: 1)) {
+                    isTextVisible.toggle()
+                }
+            }, label: {
+                Text("Toggle Text")
+                    .font(.title2)
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(Color.blue)
+                    .clipShape(.rect(cornerRadius: 10))
+            })
+            
+            if isTextVisible {
+                Text("Hello SwiftUI Animation")
+                    .font(.largeTitle)
+                    .padding()
+                    .transition(.opacity)
+                    //.transition(.scale)
+                    //.transition(.slide)
+                    //.transition(.move(edge: .leading))
+            }
+        }
     }
 }
 
