a03f55fe490a6c388b08d226e4e25daf16628645
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-15 13:12:36 +0900

    TabView 응용

--- SwiftUI/TabViewDemo/TabViewDemo/ContentView.swift
+++ SwiftUI/TabViewDemo/TabViewDemo/ContentView.swift
@@ -8,25 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var selection: Int = 0
+    @State var selection: Int = 1
     
     var body: some View {
         TabView(selection: $selection) {
-            Text("First Content View")
+            Text("Left Content View")
                 .tabItem {
-                    Image(systemName: "1.circle")
+                    Image(systemName: "arrow.left")
                     Text("Screen One")
                 }
                 .tag(0)
-            Text("Second Content View")
+            Text("Home Content View")
                 .tabItem {
-                    Image(systemName: "2.circle")
+                    Image(systemName: "house.fill")
                     Text("Screen Two")
                 }
                 .tag(1)
-            Text("Third Content View")
+            Text("Right Content View")
                 .tabItem {
-                    Image(systemName: "3.circle")
+                    Image(systemName: "arrow.right")
                     Text("Screen Three")
                 }
                 .tag(2)
