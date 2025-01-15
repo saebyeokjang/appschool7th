4ee74af78003971eae666258f838b63005e46909
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-15 14:38:08 +0900

    Gesture2

--- SwiftUI/GestureDemo/GestureDemo/ContentView.swift
+++ SwiftUI/GestureDemo/GestureDemo/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    let tap = TapGesture(count: 2)
+        .onEnded { _ in
+            print("Double Tap") }
+    
+    let longPress = LongPressGesture()
+        .onEnded { _ in
+            print("Long Press") }
+    
+    @State var magnificationScale: CGFloat = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        let magnification = MagnificationGesture(minimumScaleDelta: 1)
+            .onChanged({ value in
+                print("Magnifying Change: \(value)")
+                self.magnificationScale = value
+            })
+            .onEnded { _ in
+                print("Magnifying End")
+            }
+        
+        Image(systemName: "hand.point.right.fill")
+            .font(.largeTitle)
+            .gesture(tap)
+            .gesture(longPress)
+            .gesture(magnification)
+            .scaleEffect(magnificationScale)
+            .frame(width: 100, height: 90)
     }
 }
 
