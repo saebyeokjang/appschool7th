0003fa478651d8147b18d3afd9b98c197a6bba12
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-16 14:11:35 +0900

    검색기능 추가

---
+++ SwiftUI/CoreDataDemo/ResultsView.swift
@@ -0,0 +1,35 @@
+//
+//  ResultsView.swift
+//  CoreDataDemo
+//
+//  Created by 장새벽 on 1/16/25.
+//
+
+import SwiftUI
+import CoreData
+
+struct ResultsView: View {
+    var name: String
+    var viewContext: NSManagedObjectContext
+    
+    @State var matches: [Product]?
+    
+    var body: some View {
+        VStack {
+            List {
+                ForEach(matches ?? []) { match in
+                    Text(match.name ?? "Not found")
+                    Spacer()
+                    Text(match.quantity ?? "Not found")
+                }
+            }
+            .navigationTitle("Results")
+        }
+        .task {
+            let fetchRequest: NSFetchRequest<Product> = Product.fetchRequest()
+            fetchRequest.entity = Product.entity()
+            fetchRequest.predicate = NSPredicate(format: "name CONTAINS %@", name)
+            matches = try? viewContext.fetch(fetchRequest)
+        }
+    }
+}
