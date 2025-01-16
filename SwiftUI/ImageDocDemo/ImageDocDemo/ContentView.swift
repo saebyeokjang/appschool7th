661d089ff23d502e51b093cd0f67401766bfc7f3
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-16 11:31:21 +0900

    엔티티 디스크립션 정의하기

--- SwiftUI/ImageDocDemo/ImageDocDemo/ContentView.swift
+++ SwiftUI/ImageDocDemo/ImageDocDemo/ContentView.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @Binding var document: ImageDocDemoDocument
+    @State private var ciFilter = CIFilter.sepiaTone()
+    
+    let context = CIContext()
 
     var body: some View {
         VStack {
@@ -17,13 +22,27 @@ struct ContentView: View {
                 .aspectRatio(contentMode: .fit)
                 .padding()
             Button(action: {
-                
+                filterImage()
             }, label: {
-                Text("Save")
+                Text("Filter Image")
             })
             .padding()
         }
     }
+    
+    func filterImage() {
+        ciFilter.intensity = Float(1.0)
+        
+        let ciImage = CIImage(image: document.image)
+        ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        
+        guard let outputImage = ciFilter.outputImage else { return }
+        
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            document.image = UIImage(cgImage: cgImage)
+        }
+    }
+    
 }
 
 #Preview {
