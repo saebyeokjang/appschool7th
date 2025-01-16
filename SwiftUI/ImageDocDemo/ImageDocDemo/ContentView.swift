20c1987582e1cec3e94e1d39bc55b0893b8d936a
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-16 10:00:13 +0900

    Document 타입 변경

---
+++ SwiftUI/ImageDocDemo/ImageDocDemo/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ImageDocDemo
+//
+//  Created by 장새벽 on 1/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @Binding var document: ImageDocDemoDocument
+
+    var body: some View {
+        TextEditor(text: $document.text)
+    }
+}
+
+#Preview {
+    ContentView(document: .constant(ImageDocDemoDocument()))
+}
