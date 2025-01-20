6ffc7ec6746076ef83af88fe5baa7c62f021f767
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-20 14:04:18 +0900

    코드 리팩토링

--- SwiftUI/TodoApp/TodoApp/ContentView.swift
+++ SwiftUI/TodoApp/TodoApp/ContentView.swift
@@ -8,53 +8,34 @@
 import SwiftUI
 import SwiftData
 
-import SwiftUI
-import SwiftData
-
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var todos: [TodoItem]
     
     @State private var showingAddTodo = false
-
+    
     var body: some View {
         NavigationStack {
-            List {
-                ForEach(todos) { item in
-                    NavigationLink {
-                        TodoDetailView(item: item)
-                    } label: {
-                        Text("\(item.title) at \(item.createdAt, format: Date.FormatStyle(date: .numeric, time: .standard))")
+            TodoListView()
+                .navigationTitle("Todo List")
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        EditButton()
+                    }
+                    ToolbarItem {
+                        Button(action: {
+                            showingAddTodo = true
+                        }) {
+                            Label("Add Item", systemImage: "plus")
+                        }
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .navigationTitle("Todo List")
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: {
-                        showingAddTodo = true
-                    }) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
         }
         .sheet(isPresented: $showingAddTodo) {
             AddTodoView()
         }
     }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(todos[index])
-            }
-        }
-    }
+    
 }
 
 #Preview {
---
+++ SwiftUI/TodoApp/TodoApp/TodoListView.swift
@@ -0,0 +1,37 @@
+//
+//  TodoListView.swift
+//  TodoApp
+//
+//  Created by Saebyeok Jang on 1/20/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct TodoListView: View {
+    @Environment(\.modelContext) private var modelContext
+    
+    @Query private var todos: [TodoItem]
+    
+    var body: some View {
+        List {
+            ForEach(todos) { item in
+                TodoRowView(item: item)
+            }
+            .onDelete(perform: deleteItems)
+        }
+    }
+    
+    private func deleteItems(offsets: IndexSet) {
+        withAnimation {
+            for index in offsets {
+                modelContext.delete(todos[index])
+            }
+        }
+    }
+    
+}
+
+#Preview {
+    TodoListView()
+}
