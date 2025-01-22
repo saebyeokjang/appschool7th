e3d21b433c0a64f3484d8191ee86a28b6c60b900
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-22 14:47:23 +0900

    WeatherWidgetView 구성, 타임라인 프로바이더 변경

--- SwiftUI/WidgetDemo/WeatherWidget/WeatherWidget.swift
+++ SwiftUI/WidgetDemo/WeatherWidget/WeatherWidget.swift
@@ -9,26 +9,17 @@ import WidgetKit
 import SwiftUI
 
 struct Provider: AppIntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationAppIntent())
+    func placeholder(in context: Context) -> WeatherEntry {
+        WeatherEntry(date: Date(), city: "London", temperature: 89, description: "Thunder Storm", icon: "cloud.bolt.rain", image: "thunder")
     }
 
-    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: configuration)
+    func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> WeatherEntry {
+        WeatherEntry(date: Date(), city: "London", temperature: 89, description: "Thunder Storm", icon: "cloud.bolt.rain", image: "thunder")
     }
     
-    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
-        var entries: [SimpleEntry] = []
+    func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<WeatherEntry> {
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
-        return Timeline(entries: entries, policy: .atEnd)
+        return Timeline(entries: londonTimeline, policy: .atEnd)
     }
 
 //    func relevances() async -> WidgetRelevances<ConfigurationAppIntent> {
@@ -36,22 +27,37 @@ struct Provider: AppIntentTimelineProvider {
 //    }
 }
 
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let configuration: ConfigurationAppIntent
-}
-
 struct WeatherWidgetEntryView : View {
     var entry: Provider.Entry
 
     var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Favorite Emoji:")
-            Text(entry.configuration.favoriteEmoji)
+        ZStack {
+            Color("WeatherBackgroundColor")
+            WeatherSubView(entry: entry)
         }
+    }
+}
+
+struct WeatherSubView: View {
+    var entry: WeatherEntry
+    
+    var body: some View {
+        VStack {
+            VStack {
+                Text("\(entry.city)")
+                    .font(.title)
+                Image(systemName: entry.icon)
+                Text("\(entry.description)")
+                    .frame(minWidth: 125, minHeight: nil)
+            }
+            .padding(.bottom, 2)
+            .background(ContainerRelativeShape()
+                .fill(Color("weatherInsetColor")))
+            
+            Label("\(entry.temperature)°F", systemImage: "thermometer")
+        }
+        .foregroundColor(.white)
+        .padding()
     }
 }
 
@@ -83,6 +89,6 @@ extension ConfigurationAppIntent {
 #Preview(as: .systemSmall) {
     WeatherWidget()
 } timeline: {
-    SimpleEntry(date: .now, configuration: .smiley)
-    SimpleEntry(date: .now, configuration: .starEyes)
+    londonTimeline[0]
+    miamiTimeline[0]
 }
