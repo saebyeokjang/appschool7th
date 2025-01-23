f6771fd0af19ba8e34914e8c0431c8ef8ace02a2
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-23 15:28:51 +0900

    Remove submodule and convert to regular directory

---
+++ HostingControllerDemo/HostingControllerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HostingControllerDemo
+//
+//  Created by Saebyeok Jang on 1/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
