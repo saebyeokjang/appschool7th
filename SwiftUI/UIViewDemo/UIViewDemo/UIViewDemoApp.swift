57f46c8ee4ff102dea0cefc6be2e8d83733ee598
saebyeokjang <dev.saebyeok@gmail.com>
2025-01-23 13:43:18 +0900

    test

---
+++ SwiftUI/UIViewDemo/UIViewDemo/UIViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewDemoApp.swift
+//  UIViewDemo
+//
+//  Created by Saebyeok Jang on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct UIViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
