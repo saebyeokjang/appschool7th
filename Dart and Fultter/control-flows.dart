617483d39c84175f870291d3934ce6d95b201c06
saebyeokjang <dev.saebyeok@gmail.com>
2025-02-10 11:47:12 +0900

    switch 문 예제 추가 및 다양한 조건 비교 구현

--- Dart and Fultter/control-flows.dart
+++ Dart and Fultter/control-flows.dart
@@ -20,4 +20,43 @@ void main() {
   do {
     print('Hello 2');
   } while (isTrue);
+
+  // switch statement: 다양한 조건을 비교할 때 사용
+  int myValue = 2;
+
+  switch (myValue) {
+    case 1:
+      print('Monday');
+    case 2:
+      print('Tuesday');
+    default:
+      print('Error: Value not defined?');
+      break;
+  }
+
+  // 슬래시, 별, 쉼표, 세미콜론 등이 상수 변수인 경우...
+  const slash = '/';
+  const star = '*';
+  const plus = '+';
+  const minus = '-';
+
+  String charCode = '1';
+
+  switch (charCode) {
+    case slash || star || plus || minus:
+      print('Mathematical operator');
+    default:
+      print('Unknown');
+  }
+
+  int numValue = 5;
+
+  switch (numValue) {
+    // 0부터 9까지의 숫자인 경우, 비교 연산자와 논리 연산자를 사용할 수 있음
+    case >= 0 && <= 9:
+      print('Single digit number');
+    default:
+      print('Unknown');
+  }
+}
 }
