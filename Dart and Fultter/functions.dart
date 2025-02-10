9b6ab78f4871e6f075b2cc86300caf3032c8c610
saebyeokjang <dev.saebyeok@gmail.com>
2025-02-10 14:27:18 +0900

    익명 함수 및 화살표 함수

--- Dart and Fultter/functions.dart
+++ Dart and Fultter/functions.dart
@@ -8,11 +8,50 @@ void main() {
   var hourDifference = -7;
   getCurrentDateTimeWithDifference(hourDifference);
 
+  // 파라미터 생략 가능한 함수 호출
   printGreetingNamed();
   printGreetingNamed(personName: "Rich");
   printGreetingNamed(personName: "Mary", clientId: 001);
+
+  // 실습: 반환 값이 있는 함수
+  DateTime timeNow = getTimeDifference(0);
+  DateTime timeDifference = getTimeDifference(-7);
+
+  print('Current time: $timeNow');
+  print('Time with Difference: $timeDifference');
+
+  // 익명 함수
+  int value = 5;
+
+  // 화살표 함수
+  int ex1Squared(num1) => num1 * num1;
+  int ex1Cubed(num1) => num1 * num1 * num1;
+
+  // 중괄호 일반 함수
+  int ex2Squared(num2) {
+    return num2 * num2;
+  }
+
+  int ex2Cubed(num2) {
+    return num2 * num2 * num2;
+  }
+
+  print('EX1-Squared: ${ex1Squared(value)}');
+  print('EX1-Cubed: ${ex1Cubed(value)}');
+
+  print('EX2-Squared: ${ex2Squared(value)}');
+  print('EX2-Cubed: ${ex2Cubed(value)}');
 }
 
+// 함수 선언: 반환 값이 있는 함수
+DateTime getTimeDifference(int hourDifference) {
+  DateTime timeNow = DateTime.now();
+  DateTime timeDifference = timeNow.add(Duration(hours: hourDifference));
+
+  return timeDifference;
+}
+
+// 이름이 있는 파라미터 함수 선언, 기본값 설정
 void printGreetingNamed({String personName = 'Stranger', int clientId = 999}) {
   if (personName.contains('Stranger')) {
     print('Employee: $clientId, Stranger danger!');
