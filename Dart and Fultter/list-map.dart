e6b4869bdf2af6d4b0a586ffc8021337cdcb42d0
saebyeokjang <dev.saebyeok@gmail.com>
2025-02-11 09:22:48 +0900

    리스트심화: 요소 추가, 제거 및 필터링 기능 구현

--- Dart and Fultter/list-map.dart
+++ Dart and Fultter/list-map.dart
@@ -17,4 +17,24 @@ void main() {
 
   // 리스트의 길이 확인
   print(months.length); // 4 출력
+
+  // List advanced
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  // 특정 인덱스에 요소 추가
+  numbers.insert(2, 10); // 1, 2, 10, 3, 4, 5
+
+  // 값 제거
+  numbers.remove(4); // 1, 2, 10, 3, 5
+
+  // 값 제거 (인덱스 사용)
+  numbers.removeAt(2); // 1, 2, 3, 5
+
+  // 조건에 따라 필터링
+  List<int> evenNumbers = numbers.where((number) => number % 2 == 0).toList();
+  print(evenNumbers); // 2 출력
+
+  // 각 요소를 제곱하여 새로운 리스트 생성
+  List<int> squaredNumbers = numbers.map((number) => number * number).toList();
+  print(squaredNumbers); // 1, 4, 9, 25 출력
 }
