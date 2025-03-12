8349dd71a27821be82e7b22d3ba0c29953d0e1bc
saebyeokjang <dev.saebyeok@gmail.com>
2025-03-12 13:47:49 +0900

    SecondViewController 생명주기 메서드에 로그 추가 및 ViewController의 불필요한 로그 제거

--- UIKit/HelloWorld/HelloWorld/SecondViewController.swift
+++ UIKit/HelloWorld/HelloWorld/SecondViewController.swift
@@ -11,11 +11,38 @@ class SecondViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    // Do any additional setup after loading the view.
+    print("01 SecondViewController.viewDidLoad()")
     setupUI()
   }
 
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    print("02 SecondViewController.viewWillAppear()")
+  }
+
+  override func viewIsAppearing(_ animated: Bool) {
+    super.viewIsAppearing(animated)
+    print("03 SecondViewController.viewIsAppearing()")
+  }
+
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    print("04 SecondViewController.viewDidAppear()")
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    print("05 SecondViewController.viewWillDisappear()")
+  }
+
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    print("06 SecondViewController.viewDidDisappear()")
+  }
+
   func setupUI() {
+    print("07 SecondViewController.setupUI()")
     // SecondViewController의 배경색을 노란색으로 설정
     self.view.backgroundColor = .yellow
 
--- UIKit/HelloWorld/HelloWorld/ViewController.swift
+++ UIKit/HelloWorld/HelloWorld/ViewController.swift
@@ -12,38 +12,11 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
-    print("1 ViewController.viewDidLoad()")
 
     setupUI()
   }
 
-  override func viewWillAppear(_ animated: Bool) {
-    super.viewWillAppear(animated)
-    print("2 ViewController.viewWillAppear()")
-  }
-
-  override func viewIsAppearing(_ animated: Bool) {
-    super.viewIsAppearing(animated)
-    print("3 ViewController.viewIsAppearing()")
-  }
-
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-    print("4 ViewController.viewDidAppear()")
-  }
-
-  override func viewWillDisappear(_ animated: Bool) {
-    super.viewWillDisappear(animated)
-    print("5 ViewController.viewWillDisappear()")
-  }
-
-  override func viewDidDisappear(_ animated: Bool) {
-    super.viewDidDisappear(animated)
-    print("6 ViewController.viewDidDisappear()")
-  }
-
   func setupUI() {
-    print("7 ViewController.setupUI()")
     let label = UILabel()
     label.text = "Hello, World!"
     // (content layout) 라벨의 텍스트를 가운데 정렬
