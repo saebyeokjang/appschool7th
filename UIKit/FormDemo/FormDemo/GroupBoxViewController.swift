70ce3bac0a13ef26417b1a4522f935d98fd21146
saebyeokjang <dev.saebyeok@gmail.com>
2025-03-17 09:24:32 +0900

    FormDemo 생성

---
+++ UIKit/FormDemo/FormDemo/GroupBoxViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FormDemo
+//
+//  Created by Saebyeok Jang on 3/17/25.
+//
+
+import UIKit
+
+class GroupBoxViewController: UIViewController {
+    
+    var flag = false
+    let groupBox = UIView()
+    let groupBoxLabel = UILabel()
+    let toggle = UISwitch()
+    let textField = UITextField()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
