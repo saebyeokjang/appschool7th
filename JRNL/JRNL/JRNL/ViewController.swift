07e8537872be345215585468ca06ff664f41c518
saebyeokjang <dev.saebyeok@gmail.com>
2025-03-25 10:19:08 +0900

    Create: JRNL Project

---
+++ JRNL/JRNL/JRNL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JRNL
+//
+//  Created by Saebyeok Jang on 3/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
