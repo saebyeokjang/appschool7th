c98ba28dfaef2a8684a3c62dc0d228084671b0a4
saebyeokjang <dev.saebyeok@gmail.com>
2025-04-07 10:12:20 +0900

    Combine 실습: 회원가입 폼 및 뷰모델 추가

---
+++ Combine/CombineDemo/CombineDemo/CombineDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineDemoApp.swift
+//  CombineDemo
+//
+//  Created by Saebyeok Jang on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CombineDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Combine/CombineDemo/CombineDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CombineDemo
+//
+//  Created by Saebyeok Jang on 4/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
