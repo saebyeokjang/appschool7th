5b77a81fc76f3703206485a4b1cc254acb72bd5e
saebyeokjang <dev.saebyeok@gmail.com>
2025-04-21 10:44:30 +0900

    타겟 및 프로젝트 구조 개선을 위한 코드 리팩토링

--- Tuist-TCA/HelloTuist/Project.swift
+++ Tuist-TCA/HelloTuist/Project.swift
@@ -1,36 +1,40 @@
 import ProjectDescription
 
+let appTarget = Target.target(
+    name: "HelloTuist",
+    destinations: .iOS,
+    product: .app,
+    bundleId: "io.tuist.HelloTuist",
+    infoPlist: .extendingDefault(
+        with: [
+            "UILaunchScreen": [
+                "UIColorName": "",
+                "UIImageName": "",
+            ],
+        ]
+    ),
+    sources: ["HelloTuist/Sources/**"],
+    resources: ["HelloTuist/Resources/**"],
+    dependencies: [
+        .package(product: "Alamofire"),
+    ]
+)
+
+let testTarget = Target.target(
+    name: "HelloTuistTests",
+    destinations: .iOS,
+    product: .unitTests,
+    bundleId: "io.tuist.HelloTuistTests",
+    infoPlist: .default,
+    sources: ["HelloTuist/Tests/**"],
+    resources: [],
+    dependencies: [.target(name: "HelloTuist")]
+)
+
 let project = Project(
     name: "HelloTuist",
     targets: [
-        .target(
-            name: "HelloTuist",
-            destinations: .iOS,
-            product: .app,
-            bundleId: "io.tuist.HelloTuist",
-            infoPlist: .extendingDefault(
-                with: [
-                    "UILaunchScreen": [
-                        "UIColorName": "",
-                        "UIImageName": "",
-                    ],
-                ]
-            ),
-            sources: ["HelloTuist/Sources/**"],
-            resources: ["HelloTuist/Resources/**"],
-            dependencies: [
-                 .package(product: "Alamofire"),
-             ]
-        ),
-        .target(
-            name: "HelloTuistTests",
-            destinations: .iOS,
-            product: .unitTests,
-            bundleId: "io.tuist.HelloTuistTests",
-            infoPlist: .default,
-            sources: ["HelloTuist/Tests/**"],
-            resources: [],
-            dependencies: [.target(name: "HelloTuist")]
-        ),
+        appTarget,
+        testTarget
     ]
 )
